9f755bae8fded4e63fa69aac67ac0a5b09f167a2
Cristian <CristianGualteros23@gmail.com>
2021-06-23 13:46:50 -0500

    add new routes

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class BalanceController extends Controller
 {
-    //
+    /* public function show(Request $request)
+    {
+        $account_id =  $request->input('account_id');
+        $account = Account::findOrFail($account_id);
+
+        return $account->balance;
+    } */
 }
